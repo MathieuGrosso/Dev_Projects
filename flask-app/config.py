abba0252ca03a7ed8cab1aaa2d1774e0bff0380e
Mathieu Grosso <mathieu.grosso@hec.edu>
2022-09-26 10:39:58 +0200

    add new projects

---
+++ flask-app/config.py
@@ -0,0 +1,9 @@
+import os
+SECRET_KEY = '#d#JCqTTW\nilK\\7m\x0bp#\tj~#H'
+
+FB_APP_ID = 822721912503587
+
+# Database initialization
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
--- la-maison-jungle/src/components/Banner.js
+++ la-maison-jungle/src/components/Banner.js
@@ -1,9 +1,7 @@
-
 import '../styles/Banner.css'
-function Banner() {
-    return <div className='lmj-banner'>
-    <h1>La maison jungle</h1>
-</div>
+
+function Banner({ children }) {
+	return <div className='lmj-banner'>{children}</div>
 }
 
 export default Banner
